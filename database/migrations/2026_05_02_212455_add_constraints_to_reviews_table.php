c29f888a25f9aa20fbb7d43cc52ff06b58748e99
devano <elazzouziadnan4@gmil.com>
2026-05-02 22:28:18 +0100

    reviews pt2

--- database/migrations/2026_05_02_212455_add_constraints_to_reviews_table.php
+++ database/migrations/2026_05_02_212455_add_constraints_to_reviews_table.php
@@ -4,25 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('reviews', function (Blueprint $table) {
-            //
+            $table->tinyInteger('rating')->unsigned()->change();
+            $table->unique(['booking_id', 'client_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('reviews', function (Blueprint $table) {
-            //
+            $table->dropUnique(['booking_id', 'client_id']);
         });
     }
 };
